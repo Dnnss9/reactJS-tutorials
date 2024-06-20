35a1ee0fbc373138c987f0b7e953df692f4d33b7
Dou <houadriadounia@gmail.com>
2024-06-20 17:44:57 +0200

    reactJS tutorials

---
+++ hello-world/src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import LifecycleA from './components/LifecycleA';
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <LifecycleA/>
+      </div>
+    );
+  }
+  }
+  
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting';
+// import NameList from './components/NameList';
+// import Stylesheet from './components/Stylesheet';
+// import Inline from './components/Inline';
+// import './appStyles.css'
+// import styles from './appStyles.module.css'
+// import Form from './components/Form';
+{/* <Form/>
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.sucess }>Sucess</h1>
+        <Inline/>
+        <Stylesheet primary= {true}/>
+        <NameList/>
+        <UserGreeting/>
+        <ParentComponent/>
+        <EventBind/>
+        <FunctionClick/>
+        <ClassClick/>
+        <Counter/>
+        <Message/>
+        <Greet name = 'Bruce' heroName = 'Batman' >
+          <p>This is children props </p>
+        </Greet>
+        <Greet name = 'Clark' heroName = 'Superman' >
+          <button>Action</button>
+        </Greet>
+        <Greet name = 'Diana' heroName = 'Wonder Woman' />
+        
+        <Welcome name = 'Bruce' heroName = 'Batman'/>
+        <Welcome name = 'Clark' heroName = 'Superman'/>
+        <Welcome name = 'Diana' heroName = 'Wonder Woman' />
+       */}
